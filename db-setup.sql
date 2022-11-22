f12d5ecc588cc547f4de116878c0ee4b7ad09fbc
EricTrainingRev <eric.suminski@revature.com>
2022-11-22 15:26:35 -0500

    Changed sb setup formatting

--- db-setup.sql
+++ db-setup.sql
@@ -3,17 +3,17 @@
 create table users(
 	id serial primary key,
 	username varchar(20) unique,
-	"password" varchar(20)
+	password varchar(20)
 );
 
 create table planets(
 	id serial primary key,
-	"name" varchar(20),
+	name varchar(20),
 	ownerId int references users(id)
 );
 
 create table moons(
 	id serial primary key,
-	"name" varchar(20),
+	name varchar(20),
 	myPlanetId int references planets(id)
 );
