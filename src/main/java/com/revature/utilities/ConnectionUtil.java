c1ece2d231661857c4f6c8d84d841ea7e09f7a4d
EricTrainingRev <eric.suminski@revature.com>
2022-11-22 10:51:11 -0500

    Added a utility class to handle the database connection

---
+++ src/main/java/com/revature/utilities/ConnectionUtil.java
@@ -0,0 +1,23 @@
+package com.revature.utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    
+    public static Connection createConnection(){
+        try {
+            return DriverManager.getConnection(System.getenv("URL"), System.getenv("USERNAME"), System.getenv("PASSWORD"));
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        // use this to confirm you set up your environment variables correctly
+        System.out.println(createConnection());
+    }
+
+}
