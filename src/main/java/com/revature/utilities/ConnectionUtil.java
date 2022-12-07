b42f92871ff6ae32bdf2e6811c349e2d7b8e8e03
EricTrainingRev <eric.suminski@revature.com>
2022-12-07 17:14:36 -0500

    added postgres dependency and switch connection util from environment variables to properties implementation

--- src/main/java/com/revature/utilities/ConnectionUtil.java
+++ src/main/java/com/revature/utilities/ConnectionUtil.java
@@ -1,19 +1,25 @@
 package com.revature.utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionUtil {
     
     public static Connection createConnection(){
         try {
+            Properties prop = new Properties();
+            prop.load(new FileInputStream("src/main/resources/db-properties.properties"));
             return DriverManager.getConnection(
-                System.getenv("URL"), 
-                System.getenv("USERNAME"), 
-                System.getenv("PASSWORD")
+                prop.getProperty("URL"), 
+                prop.getProperty("USERNAME"), 
+                prop.getProperty("PASSWORD")
             );
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             System.out.println(e.getMessage());
             return null;
         }
