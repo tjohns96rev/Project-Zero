2d861cf5054452d726855de56d62bb74c870b709
EricTrainingRev <eric.suminski@revature.com>
2022-12-08 10:27:37 -0500

    Added Jackson Databind to pom and added Javalin dev logging

--- src/main/java/com/revature/MainDriver.java
+++ src/main/java/com/revature/MainDriver.java
@@ -1,5 +1,6 @@
 package com.revature;
 
+
 import com.revature.controller.RequestMapping;
 
 import io.javalin.Javalin;
@@ -7,7 +8,9 @@ import io.javalin.Javalin;
 public class MainDriver {
 
 	public static void main(String[] args) {
-		Javalin app = Javalin.create();
+		Javalin app = Javalin.create(confg ->{
+			confg.plugins.enableDevLogging();
+		});
 		RequestMapping.setupEndpoints(app);
 		app.start(7000);
 	}
